39089c8e4a3c0230895b375488cf3593e8d4a330
Rochel Rodrigues Coelho <rochelrrc@gmail.com>
2023-04-10 17:07:41 -0300

    teste de operacoes aritimeticas

---
+++ Variaveis/operadores-aritimeticos.js
@@ -0,0 +1,16 @@
+let x = 10, y = 5;
+
+let resultado = x + y;
+console.log(`A soma de ${x} mais ${y} é igual a ${resultado}`);
+console.log(" ");
+
+resultado = x - y;
+console.log(`A diferença de ${x} e ${y} é igual a ${resultado}`);
+console.log(" ");
+
+resultado = x * y;
+console.log(`O produto de ${x} e ${y} é igual a ${resultado}`);
+console.log(" ");
+
+resultado = x / y;
+console.log(`A divisão de ${x} por ${y} é igual a ${resultado}`);
