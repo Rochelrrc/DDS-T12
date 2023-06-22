89f2c5d690d91b71fdb29cfa28667486992f833e
Rochel Rodrigues Coelho <rochelrrc@gmail.com>
2023-06-22 12:03:28 -0300

    Feito projeto de menu com DOM

---
+++ Menu DOM/js/script.js
@@ -0,0 +1,57 @@
+const btnUnderline = document.querySelector(".btn-underline");
+const btnItalic = document.querySelector(".btn-italic");
+const btnBold = document.querySelector(".btn-bold");
+
+const btnDecrementFont = document.querySelector(".btn-decrement-font");
+const currentFontSize = document.querySelector(".current-font-size");
+const btnIncrementFont = document.querySelector(".btn-increment-font");
+
+const btnRed = document.querySelector(".btn-red");
+const btnBlue = document.querySelector(".btn-blue");
+const btnPurple = document.querySelector(".btn-purple");
+const btnBlack = document.querySelector(".btn-black");
+
+const text = document.querySelector(".text");
+
+btnUnderline.onclick = () => {
+    text.style.textDecoration = text.style.textDecoration === "underline" ? "none" : "underline"
+};
+btnItalic.onclick = () => {
+    text.style.fontStyle = text.style.fontStyle === "italic" ? "normal" : "italic"
+};
+btnBold.onclick = () => {
+    text.style.fontWeight = text.style.fontWeight === "bold" ? "normal" : "bold"
+};
+
+function getCurrentFontSize() {
+
+    const currentFontSize = text.style.fontSize.split("px")[0];
+
+    return parseInt(currentFontSize) || 16;
+}
+
+btnDecrementFont.onclick = () => {
+    if (getCurrentFontSize() > 12) {
+        text.style.fontSize = `${getCurrentFontSize() - 2}px`
+    };
+    currentFontSize.textContent = getCurrentFontSize();
+}
+btnIncrementFont.onclick = () => {
+    text.style.fontSize = `${getCurrentFontSize() + 2}px`
+
+    currentFontSize.textContent = getCurrentFontSize();
+};
+
+
+btnRed.onclick = () => {
+    text.style.color = "red";
+};
+btnBlue.onclick = () => {
+    text.style.color = "blue";
+};
+btnPurple.onclick = () => {
+    text.style.color = "purple";
+};
+btnBlack.onclick = () => {
+    text.style.color = "black";
+};
